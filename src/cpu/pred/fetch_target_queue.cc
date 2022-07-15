6aa659ac74fc83cd905eb143975242fdb5b415ad
Zhou Yaoyang <shinezyy@qq.com>
2022-07-15 11:23:58 +0800

    cpu: extract FTQ from Decoupled BP
    Change-Id: Ia67b9297df5f1a0351df8b8958e03d28a1bf8c39

---
+++ src/cpu/pred/fetch_target_queue.cc
@@ -0,0 +1,135 @@
+#include "cpu/pred/fetch_target_queue.hh"
+
+#include "base/trace.hh"
+#include "debug/DecoupleBP.hh"
+
+namespace gem5 {
+
+namespace branch_prediction {
+
+FetchTargetQueue::FetchTargetQueue(unsigned size) : ftqSize(size)
+{
+    fetchTargetEnqState.pc = 0x80000000;
+    fetchDemandTargetId = 0;
+    supplyFetchTargetState.valid = false;
+}
+
+void
+FetchTargetQueue::squash(FetchTargetId new_enq_target_id,
+                         FetchStreamId new_enq_stream_id, Addr new_enq_pc)
+{
+    ftq.clear();
+    // Because we squash the whole ftq, head and tail should be the same
+    auto new_fetch_demand_target_id = new_enq_target_id;
+
+    fetchTargetEnqState.desireTargetId = new_enq_target_id;
+    fetchTargetEnqState.streamId = new_enq_stream_id;
+    fetchTargetEnqState.pc = new_enq_pc;
+
+    supplyFetchTargetState.valid = false;
+    fetchDemandTargetId = new_fetch_demand_target_id;
+    DPRINTF(DecoupleBP,
+            "FTQ demand stream ID update to %lu, FTQ demand pc update to "
+            "%#lx\n",
+            new_enq_stream_id,
+            new_enq_pc);
+}
+
+bool
+FetchTargetQueue::fetchTargetAvailable() const
+{
+    return supplyFetchTargetState.valid &&
+           supplyFetchTargetState.targetId == fetchDemandTargetId;
+}
+
+FtqEntry&
+FetchTargetQueue::getTarget()
+{
+    assert(fetchTargetAvailable());
+    return supplyFetchTargetState.entry;
+}
+
+void
+FetchTargetQueue::finishCurrentFetchTarget()
+{
+
+    ++fetchDemandTargetId;
+    ftq.erase(supplyFetchTargetState.targetId);
+    DPRINTF(DecoupleBP,
+            "Finish current fetch target: %lu, inc demand to %lu\n",
+            supplyFetchTargetState.targetId,
+            fetchDemandTargetId);
+}
+
+bool
+FetchTargetQueue::trySupplyFetchWithTarget()
+{
+    if (!supplyFetchTargetState.valid ||
+        supplyFetchTargetState.targetId != fetchDemandTargetId) {
+        auto it = ftq.find(fetchDemandTargetId);
+        if (it != ftq.end()) {
+            DPRINTF(DecoupleBP,
+                    "Found ftq entry with id %lu, writing to "
+                    "fetchReadFtqEntryBuffer\n",
+                    fetchDemandTargetId);
+            supplyFetchTargetState.valid = true;
+            supplyFetchTargetState.targetId = fetchDemandTargetId;
+            supplyFetchTargetState.entry = it->second;
+            return true;
+        } else {
+            DPRINTF(
+                DecoupleBP, "Target id %lu not found\n", fetchDemandTargetId);
+            if (!ftq.empty()) {
+                // sanity check
+                --it;
+                DPRINTF(DecoupleBP,
+                        "Last entry of target queue: %lu\n",
+                        it->first);
+                assert(it->first < fetchDemandTargetId);
+            }
+            return false;
+        }
+    }
+    DPRINTF(DecoupleBP,
+            "FTQ supplying, valid: %u, supply id: %u, demand id: %u\n",
+            supplyFetchTargetState.valid,
+            supplyFetchTargetState.targetId,
+            fetchDemandTargetId);
+    return true;
+}
+
+
+std::pair<bool, FetchTargetQueue::FTQIt>
+FetchTargetQueue::getDemandTargetIt()
+{
+    FTQIt it = ftq.find(fetchDemandTargetId);
+    return std::make_pair(it != ftq.end(), it);
+}
+
+void
+FetchTargetQueue::enqueue(FtqEntry entry)
+{
+    DPRINTF(DecoupleBP,
+            "Enqueueing target %lu with pc %#x and stream %lu\n",
+            fetchTargetEnqState.desireTargetId,
+            entry.startPC,
+            entry.fsqID);
+    ftq[fetchTargetEnqState.desireTargetId] = entry;
+}
+
+void
+FetchTargetQueue::dump(const char *when)
+{
+    DPRINTF(DecoupleBP, "%s, dump FTQ\n", when);
+    for (auto it = ftq.begin(); it != ftq.end(); ++it) {
+        DPRINTFR(DecoupleBP,
+                 "FTQ entry: %lu, pc: %#x, stream: %lu\n",
+                 it->first,
+                 it->second.startPC,
+                 it->second.fsqID);
+    }
+}
+
+}  // namespace branch_prediction
+
+}  // namespace gem5
---
+++ src/cpu/pred/fetch_target_queue.hh
@@ -0,0 +1,87 @@
+#ifndef __CPU_PRED_FETCH_TARGET_QUEUE_HH__
+#define __CPU_PRED_FETCH_TARGET_QUEUE_HH__
+
+#include "cpu/pred/stream_struct.hh"
+
+namespace gem5 {
+
+namespace branch_prediction {
+
+struct FetchTargetEnqState
+{
+    Addr pc;
+    FetchStreamId streamId;
+    FetchTargetId desireTargetId;
+    FetchTargetEnqState() : pc(0), streamId(1), desireTargetId(0) {}
+};
+
+struct FetchTargetReadState
+{
+    bool valid;
+    FetchTargetId targetId;
+    FtqEntry entry;
+};
+
+class FetchTargetQueue
+{
+    // todo: move fetch target buffer here
+    // 1. enqueue from fetch stream buffer
+    // 2. supply fetch with fetch target head
+    // 3. redirect fetch target head after squash
+    using FTQ = std::map<FetchTargetId, FtqEntry>;
+    using FTQIt = FTQ::iterator;
+    FTQ ftq;
+    unsigned ftqSize;
+    FetchTargetId ftqId{0};  // this is a queue ptr for ftq itself
+
+    // The supply/responsing fetch target state
+    FetchTargetReadState supplyFetchTargetState;
+    // The demanded fetch target ID to send to fetch
+    FetchTargetId fetchDemandTargetId{0};
+
+    FetchTargetEnqState fetchTargetEnqState;
+
+  public:
+    FetchTargetQueue(unsigned size);
+
+    void squash(FetchTargetId new_enq_target_id,
+                FetchStreamId new_enq_stream_id, Addr new_enq_pc);
+
+    bool fetchTargetAvailable() const;
+
+    FtqEntry &getTarget();
+
+    FetchTargetEnqState &getEnqState() { return fetchTargetEnqState; }
+
+    FetchTargetId getSupplyingTargetId()
+    {
+        return supplyFetchTargetState.targetId;
+    }
+
+    FetchStreamId getSupplyingStreamId()
+    {
+        return supplyFetchTargetState.entry.fsqID;
+    }
+
+    void finishCurrentFetchTarget();
+
+    bool trySupplyFetchWithTarget();
+
+
+    bool empty() const { return ftq.empty(); }
+
+    unsigned size() const { return ftq.size(); }
+
+    bool full() const { return ftq.size() >= ftqSize; }
+
+    std::pair<bool, FTQIt> getDemandTargetIt();
+
+    void enqueue(FtqEntry entry);
+
+    void dump(const char *when);
+};
+
+}
+}
+
+#endif  // __CPU_PRED_FETCH_TARGET_QUEUE_HH__
