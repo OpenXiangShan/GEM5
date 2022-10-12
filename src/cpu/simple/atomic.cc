62cc6e98490fe23f224c47b7bec15085ed50797f
tastynoob <934348725@qq.com>
2022-10-12 16:05:08 +0800

    cpu-simple: clean up code style and fix atomicSimpleCPU
    -modify base.cc code style
    -atomic.cc: mark strictly ordered mem ref as mmio for difftest in atomicSimpleCPU
    
    Change-Id: If0dbc2853a632f1ac1e313433b09e3c214e2e9d9

--- src/cpu/simple/atomic.cc
+++ src/cpu/simple/atomic.cc
@@ -272,6 +272,9 @@ AtomicSimpleCPU::suspendContext(ThreadID thread_num)
 Tick
 AtomicSimpleCPU::sendPacket(RequestPort &port, const PacketPtr &pkt)
 {
+    if (pkt->req->isStrictlyOrdered()) {
+        curInstStrictOrdered = true;
+    }
     return port.sendAtomic(pkt);
 }
 
@@ -702,7 +705,6 @@ AtomicSimpleCPU::tick()
                     // caused the retry are unlikely to change every tick.
                     stall_ticks += clockEdge(syscallRetryLatency) - curTick();
                 }
-
                 postExecute();
             }
 
