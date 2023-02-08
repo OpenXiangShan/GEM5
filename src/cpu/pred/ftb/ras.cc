65e694c0aa8de7a1000423ae3d78c2526604e97c
Lingrui98 <goulingrui19s@ict.ac.cn>
2023-02-08 20:12:09 +0800

    ftb-pred: add non-spec ras with db as reference

--- src/cpu/pred/ftb/ras.cc
+++ src/cpu/pred/ftb/ras.cc
@@ -11,10 +11,18 @@ RAS::RAS(const Params &p)
     numEntries(p.numEntries),
     ctrWidth(p.ctrWidth)
 {
-    sp = 0;
-    stack.resize(numEntries);
     maxCtr = (1 << ctrWidth) - 1;
-    for (auto &entry : stack) {
+    // init spec stack
+    specSp = 0;
+    specStack.resize(numEntries);
+    for (auto &entry : specStack) {
+        entry.ctr = 0;
+        entry.retAddr = 0x80000000L;
+    }
+    // init non-spec stack
+    nonSpecSp = 0;
+    nonSpecStack.resize(numEntries);
+    for (auto &entry : nonSpecStack) {
         entry.ctr = 0;
         entry.retAddr = 0x80000000L;
     }
@@ -24,7 +32,8 @@ void
 RAS::setTrace()
 {
     if (enableDB) {
-        std::vector<std::pair<std::string, DataType>> fields_vec = {
+        // record every modification to the spec-stack
+        std::vector<std::pair<std::string, DataType>> spec_fields_vec = {
             std::make_pair("condition", UINT64),
             std::make_pair("op", UINT64),
             std::make_pair("startPC", UINT64),
@@ -35,8 +44,28 @@ RAS::setTrace()
             std::make_pair("tosAddr", UINT64),
             std::make_pair("tosCtr", UINT64)
         };
-        rasTrace = _db->addAndGetTrace("RASTRACE", fields_vec);
-        rasTrace->init_table();
+        specRasTrace = _db->addAndGetTrace("SPECRASTRACE", spec_fields_vec);
+        specRasTrace->init_table();
+
+        // record every modification to the non-spec-stack, used as reference model
+        std::vector<std::pair<std::string, DataType>> nonspec_fields_vec = {
+            // real info
+            std::make_pair("op", UINT64),
+            std::make_pair("startPC", UINT64),
+            std::make_pair("brPC", UINT64),
+            std::make_pair("retAddr", UINT64),
+            // prediction info
+            std::make_pair("predSp", UINT64),
+            std::make_pair("predTosAddr", UINT64),
+            std::make_pair("predTosCtr", UINT64),
+            // before op
+            std::make_pair("sp", UINT64),
+            std::make_pair("tosAddr", UINT64),
+            std::make_pair("tosCtr", UINT64),
+            std::make_pair("miss", UINT64)
+        };
+        nonSpecRasTrace = _db->addAndGetTrace("NONSPECRASTRACE", nonspec_fields_vec);
+        nonSpecRasTrace->init_table();
     }
 }
 
@@ -44,13 +73,15 @@ void
 RAS::putPCHistory(Addr startAddr, const boost::dynamic_bitset<> &history,
                   std::vector<FullFTBPrediction> &stagePreds)
 {
+    auto &stack = specStack;
+    auto &sp = specSp;
     assert(getDelay() < stagePreds.size());
     for (int i = getDelay(); i < stagePreds.size(); i++) {
         stagePreds[i].returnTarget = stack[sp].retAddr;
     }
     meta.sp = sp;
     meta.tos = stack[sp];
-    printStack("putPCHistory");
+    printStack("putPCHistory", stack, sp);
 }
 
 std::shared_ptr<void>
@@ -63,53 +94,88 @@ RAS::getPredictionMeta()
 void
 RAS::specUpdateHist(const boost::dynamic_bitset<> &history, FullFTBPrediction &pred)
 {
+    auto &stack = specStack;
+    auto &sp = specSp;
     // do push & pops on prediction
     pred.returnTarget = stack[sp].retAddr;
     auto takenSlot = pred.getTakenSlot();
     if (takenSlot.isCall) {
         Addr retAddr = takenSlot.pc + takenSlot.size;
-        RASTrace rec(When::SPECULATIVE, RAS_OP::PUSH, pred.bbStart, takenSlot.pc, retAddr, sp, stack[sp].retAddr, stack[sp].ctr);
-        rasTrace->write_record(rec);
-        push(retAddr);
+        SpecRASTrace rec(When::SPECULATIVE, RAS_OP::PUSH, pred.bbStart, takenSlot.pc,
+            retAddr, sp, stack[sp].retAddr, stack[sp].ctr);
+        specRasTrace->write_record(rec);
+        push(retAddr, stack, sp);
     }
     if (takenSlot.isReturn) {
-        RASTrace rec(When::SPECULATIVE, RAS_OP::POP, pred.bbStart, takenSlot.pc, stack[sp].retAddr, sp, stack[sp].retAddr, stack[sp].ctr);
-        rasTrace->write_record(rec);
+        SpecRASTrace rec(When::SPECULATIVE, RAS_OP::POP, pred.bbStart, takenSlot.pc,
+            stack[sp].retAddr, sp, stack[sp].retAddr, stack[sp].ctr);
+        specRasTrace->write_record(rec);
         // do pop
-        pop();
+        pop(stack, sp);
     }
-    printStack("after specUpdateHist");
+    printStack("after specUpdateHist", stack, sp);
 }
 
 void
 RAS::recoverHist(const boost::dynamic_bitset<> &history, const FetchStream &entry, int shamt, bool cond_taken)
 {
-    printStack("before recoverHist");
+    auto &stack = specStack;
+    auto &sp = specSp;
+    printStack("before recoverHist", stack, sp);
     // recover sp and tos first
     auto meta_ptr = std::static_pointer_cast<RASMeta>(entry.predMetas[getComponentIdx()]);
     auto takenSlot = entry.exeBranchInfo;
-    RASTrace rec(When::REDIRECT, RAS_OP::RECOVER, entry.startPC, takenSlot.pc, 0, sp, stack[sp].retAddr, stack[sp].ctr);
-    rasTrace->write_record(rec);
+    SpecRASTrace rec(When::REDIRECT, RAS_OP::RECOVER, entry.startPC, takenSlot.pc, 0, sp, stack[sp].retAddr, stack[sp].ctr);
+    specRasTrace->write_record(rec);
     sp = meta_ptr->sp;
     stack[sp] = meta_ptr->tos;
 
     // do push & pops on control squash
     if (takenSlot.isCall) {
         Addr retAddr = takenSlot.pc + takenSlot.size;
-        RASTrace rec(When::REDIRECT, RAS_OP::PUSH, entry.startPC, takenSlot.pc, retAddr, sp, stack[sp].retAddr, stack[sp].ctr);
-        rasTrace->write_record(rec);
-        push(retAddr);
+        SpecRASTrace rec(When::REDIRECT, RAS_OP::PUSH, entry.startPC, takenSlot.pc, retAddr, sp, stack[sp].retAddr, stack[sp].ctr);
+        specRasTrace->write_record(rec);
+        push(retAddr, stack, sp);
     }
     if (takenSlot.isReturn) {
-        RASTrace rec(When::REDIRECT, RAS_OP::POP, entry.startPC, takenSlot.pc, stack[sp].retAddr, sp, stack[sp].retAddr, stack[sp].ctr);
-        rasTrace->write_record(rec);
-        pop();
+        SpecRASTrace rec(When::REDIRECT, RAS_OP::POP, entry.startPC, takenSlot.pc, stack[sp].retAddr, sp, stack[sp].retAddr, stack[sp].ctr);
+        specRasTrace->write_record(rec);
+        pop(stack, sp);
     }
-    printStack("after recoverHist");
+    printStack("after recoverHist", stack, sp);
 }
 
 void
-RAS::push(Addr retAddr)
+RAS::update(const FetchStream &entry)
+{
+    auto &stack = nonSpecStack;
+    auto &sp = nonSpecSp;
+    printStack("before update", stack, sp);
+    auto takenSlot = entry.exeBranchInfo;
+    if (entry.exeTaken && (takenSlot.isReturn || takenSlot.isCall)) {
+        auto meta_ptr = std::static_pointer_cast<RASMeta>(entry.predMetas[getComponentIdx()]);
+        auto pred_sp = meta_ptr->sp;
+        auto pred_tos = meta_ptr->tos;
+        auto miss = entry.squashType == SQUASH_CTRL && entry.squashPC == entry.exeBranchInfo.pc;
+        if (takenSlot.isCall) {
+            Addr retAddr = takenSlot.pc + takenSlot.size;
+            NonSpecRASTrace rec(RAS_OP::PUSH, entry.startPC, takenSlot.pc, retAddr,
+                pred_sp, pred_tos.retAddr, pred_tos.ctr, sp, stack[sp].retAddr, stack[sp].ctr, miss);
+            nonSpecRasTrace->write_record(rec);
+            push(retAddr, stack, sp);
+        }
+        if (takenSlot.isReturn) {
+            NonSpecRASTrace rec(RAS_OP::POP, entry.startPC, takenSlot.pc, takenSlot.target,
+                pred_sp, pred_tos.retAddr, pred_tos.ctr, sp, stack[sp].retAddr, stack[sp].ctr, miss);
+            nonSpecRasTrace->write_record(rec);
+            pop(stack, sp);
+        }
+    }
+    printStack("after update", stack, sp);
+}
+
+void
+RAS::push(Addr retAddr, std::vector<RASEntry> &stack, int &sp)
 {
     auto tos = stack[sp];
     if (tos.retAddr == retAddr && tos.ctr < maxCtr) {
@@ -123,7 +189,7 @@ RAS::push(Addr retAddr)
 }
 
 void
-RAS::pop()
+RAS::pop(std::vector<RASEntry> &stack, int &sp)
 {
     auto tos = stack[sp];
     if (tos.ctr > 0) {
--- src/cpu/pred/ftb/ras.hh
+++ src/cpu/pred/ftb/ras.hh
@@ -46,9 +46,12 @@ class RAS : public TimedBaseFTBPredictor
 
         void recoverHist(const boost::dynamic_bitset<> &history, const FetchStream &entry, int shamt, bool cond_taken) override;
 
+        void update(const FetchStream &entry) override;
+
         enum When {
             SPECULATIVE,
-            REDIRECT
+            REDIRECT,
+            COMMIT
         };
 
         enum RAS_OP {
@@ -60,15 +63,15 @@ class RAS : public TimedBaseFTBPredictor
 
     private:
 
-        void push(Addr retAddr);
+        void push(Addr retAddr, std::vector<RASEntry> &stack, int &sp);
 
-        void pop();
+        void pop(std::vector<RASEntry> &stack, int &sp);
 
         void ptrInc(int &ptr);
 
         void ptrDec(int &ptr);
 
-        void printStack(const char *when) {
+        void printStack(const char *when, std::vector<RASEntry> &stack, int &sp) {
             DPRINTF(FTBRAS, "printStack when %s: \n", when);
             for (int i = 0; i < numEntries; i++) {
                 DPRINTFR(FTBRAS, "entry [%d], retAddr %#lx, ctr %d", i, stack[i].retAddr, stack[i].ctr);
@@ -87,18 +90,23 @@ class RAS : public TimedBaseFTBPredictor
 
         int maxCtr;
 
-        int sp;
+        int specSp;
 
-        std::vector<RASEntry> stack;
+        int nonSpecSp;
+
+        std::vector<RASEntry> specStack;
+
+        std::vector<RASEntry> nonSpecStack;
 
         RASMeta meta;
 
-        TraceManager *rasTrace;
+        TraceManager *specRasTrace;
+        TraceManager *nonSpecRasTrace;
 
 };
 
-struct RASTrace : public Record {
-    RASTrace(RAS::When when, RAS::RAS_OP op, Addr startPC, Addr brPC,
+struct SpecRASTrace : public Record {
+    SpecRASTrace(RAS::When when, RAS::RAS_OP op, Addr startPC, Addr brPC,
         Addr retAddr, int sp, Addr tosAddr, unsigned tosCtr)
     {
         _tick = curTick();
@@ -110,6 +118,26 @@ struct RASTrace : public Record {
         _uint64_data["sp"] = sp;
         _uint64_data["tosAddr"] = tosAddr;
         _uint64_data["tosCtr"] = tosCtr;
+    }
+};
+
+struct NonSpecRASTrace : public Record {
+    NonSpecRASTrace(RAS::RAS_OP op, Addr startPC, Addr brPC, Addr retAddr,
+        int predSp, Addr predTosAddr, unsigned predTosCtr,
+        int sp, Addr tosAddr, unsigned tosCtr, bool miss)
+    {
+        _tick = curTick();
+        _uint64_data["op"] = op;
+        _uint64_data["startPC"] = startPC;
+        _uint64_data["brPC"] = brPC;
+        _uint64_data["retAddr"] = retAddr;
+        _uint64_data["predSp"] = predSp;
+        _uint64_data["predTosAddr"] = predTosAddr;
+        _uint64_data["predTosCtr"] = predTosCtr;
+        _uint64_data["sp"] = sp;
+        _uint64_data["tosAddr"] = tosAddr;
+        _uint64_data["tosCtr"] = tosCtr;
+        _uint64_data["miss"] = miss;
     }
 };
 
