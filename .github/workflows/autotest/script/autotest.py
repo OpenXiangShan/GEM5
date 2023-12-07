963cf4a571e1b296da4af40285befb593b515a22
tastynoob <934348725@qq.com>
2023-12-07 11:20:28 +0800

    ci: use spike diff and update script

--- .github/workflows/autotest/script/autotest.py
+++ .github/workflows/autotest/script/autotest.py
@@ -5,8 +5,6 @@ from io import TextIOWrapper
 from logging import warning
 import random
 import smtplib
-from email.mime.text import MIMEText
-from email.header import Header
 import subprocess
 from multiprocessing import Pool
 import os
@@ -16,11 +14,10 @@ import utils
 import psutil
 
 
-#如何添加自定义的特殊变量
-#在下面中找到Wstart,Wrun_multi,Wrun_single,Wend4个函数
-#分别对应pre-work,multi模式work,single模式work,post-work
-#在其中找到"#添加自定义特殊变量"注释,添加字典即可
-
+# 如何添加自定义的特殊变量
+# 在下面中找到Wstart,Wrun_multi,Wrun_single,Wend4个函数
+# 分别对应pre-work,multi模式work,single模式work,post-work
+# 在其中找到"#添加自定义特殊变量"注释,添加字典即可
 
 
 parser = argparse.ArgumentParser(description='specify a cfg file')
@@ -39,29 +36,17 @@ if len(works) == 0:
     exit(1)
 
 
-
 if not os.path.exists(cfgfile['global']['log_root']):
     os.makedirs(cfgfile['global']['log_root'])
-#初始化tpoolId
+# 初始化tpoolId
 utils.tpool_init(cfgfile)
 
 
-
-def mailSendMsg(msg: str):
+def SendMsg(msg: str):
     if cfgfile['global']['debug_mode'] == 'true':
         print(msg)
-    elif cfgfile['global']['debug_mode'] == 'false' and cfgfile['mail']['enable'] == 'true':
-        try:
-            smtp = smtplib.SMTP()
-            smtp.connect(cfgfile['mail']['mail_host'])
-            smtp.login(cfgfile['mail']['mail_sender'],
-                       cfgfile['mail']['mail_license'])
-            for receiver in cfgfile['mail']['mail_receivers'].split(';'):
-                smtp.sendmail(cfgfile['mail']['mail_sender'], receiver, msg)
-            smtp.quit()
-        except Exception as e:
-            print(msg)
-            warning("mail send fail!!,check your license and network")
+    elif cfgfile['global']['debug_mode'] == 'false':
+        pass
 
 
 def startMain(work, log_dir: str, etcArg):
@@ -70,7 +55,7 @@ def startMain(work, log_dir: str, etcArg):
     log_ = log_dir
     #
     numaCores = cfgfile['work-'+work[0]].get('numacores')
-    numa_args,C = utils.tpool_alloc(numaCores)
+    numa_args, C = utils.tpool_alloc(numaCores)
     #
     utils.argReplace(task, dict({'sublog': log_, 'numa': numa_args}, **etcArg))
     if not os.path.exists(log_):
@@ -100,7 +85,8 @@ def startMain(work, log_dir: str, etcArg):
                               stderr=subprocess.STDOUT, stdin=None, check=False, encoding='utf-8')
     tupTime = int(time.time()-startTime)  # 秒时间戳
     other.write(
-        '**********runTime:{0}h:{1}m:{2}s**********\n'.format(tupTime // 3600, (tupTime % 3600)//60, (tupTime % 3600)%60))
+        '**********runTime:{0}h:{1}m:{2}s**********\n'.format(
+            tupTime // 3600, (tupTime % 3600)//60, (tupTime % 3600) % 60))
     # start except-task
     if not (post and post.returncode == 0):
         other.write(
@@ -108,14 +94,9 @@ def startMain(work, log_dir: str, etcArg):
         other.flush()
         exce = subprocess.run(args=task[3], shell=True, stdout=other,
                               stderr=subprocess.STDOUT, stdin=None, check=False, encoding='utf-8')
+        SendMsg(
+            """error in:[work:{0}][log:{1}]""".format(work[0], log_))
         other.close()
-        mailSendMsg(
-            """autotest find a error in:
-            work:{0}
-            log:{1}
-            """.format(work[0],
-                       log_
-                       ))
         utils.tpool_free(C)
         return False
     other.close()
@@ -161,8 +142,8 @@ def Wrun_multi(log_dir, etcArg):
         random_int = random.randint(0, 10000)
         results.append(pool.apply_async(
             startMain,
-            (work, log_dir+'/'+work[0],
-             dict({'tid': cnt, 'random_int': random_int}, **etcArg))))#添加自定义特殊变量
+            (work, log_dir+'/'+work[0],  # 添加自定义特殊变量
+             dict({'tid': cnt, 'random_int': random_int}, **etcArg))))
         cnt += 1
         if cnt >= int(cfgfile['iteration']['max_process']):
             cnt = 0
@@ -195,7 +176,7 @@ def Wrun_single(log_dir, etcArg):
                     startMain,
                     (work,
                      log_dir+'/'+work[0]+'/'+sublog[i],
-                     dict({'tid': cnt, 'random_int': random_int, 'binfile': files[i]}, **etcArg))))  # 添加自定义特殊变量
+                     dict({'workname' : work[0], 'tid': cnt, 'random_int': random_int, 'binfile': files[i]}, **etcArg))))  # 添加自定义特殊变量
             cnt += 1
             if cnt >= int(cfgfile['iteration']['max_process']):
                 cnt = 0
@@ -210,8 +191,9 @@ def Wrun_single(log_dir, etcArg):
             finished = False
     return finished, runErr_works
 
-#发生任何错误,均会直接执行except-task
-#返回post-work是否执行正确,如果前面任务执行错误,则直接执行post-work的except-task
+# 发生任何错误,均会直接执行except-task
+# 返回post-work是否执行正确,如果前面任务执行错误,则直接执行post-work的except-task
+
 def Wend(work_finished, log_dir, log_file: TextIOWrapper, etcArg: dict):
     '''
     执行post-work
@@ -221,10 +203,11 @@ def Wend(work_finished, log_dir, log_file: TextIOWrapper, etcArg: dict):
     log_file.write('**********post-work:task start**********\n')
     log_file.flush()
     # post-work:task start
-    ret = None 
+    ret = None
     if work_finished:
         ret = subprocess.run(args=task[1], shell=True, stdout=log_file,
-                            stderr=subprocess.STDOUT, stdin=None, check=False, encoding='utf-8')
+                             stderr=subprocess.STDOUT, stdin=None,
+                             check=False, encoding='utf-8')
     # post-work:post-task start
     post = None
     if ret and ret.returncode == 0:
@@ -241,22 +224,19 @@ def Wend(work_finished, log_dir, log_file: TextIOWrapper, etcArg: dict):
     return True
 
 
-
-
 def iteration():
     work_log_path = cfgfile['global']['log_root']
     if not os.path.exists(work_log_path):
         os.mkdir(work_log_path)
     work_log_file = open(work_log_path + '/iter_log.txt', 'w')
-    etcArg = {}
-    ignore_return = False
+    etcArg = cfgfile['global']
     error_msg = ''
     #######
     finished0 = Wstart(work_log_path, work_log_file, etcArg)
     finished1 = False
     if finished0:
         #######
-        runErr_works=""
+        runErr_works = ""
         if cfgfile['iteration']['working_mode'] == 'multi':
             finished1, runErr_works = Wrun_multi(work_log_path, etcArg)
         elif cfgfile['iteration']['working_mode'] == 'single':
@@ -265,27 +245,19 @@ def iteration():
             print("the error working_mode!")
             exit(1)
         if not finished1:
-            error_msg += 'error works:{0}\n'.format(str(runErr_works))
+            error_msg += 'found {0} works error'.format(len(runErr_works))
     else:
         error_msg += 'pre-work running error\n'
     # 它会自动关闭work_log_file
     finished2 = Wend(finished1, work_log_path,
-                        work_log_file, etcArg)
-    if finished1 and (not finished2):#post-work执行发生错误
+                     work_log_file, etcArg)
+    if finished1 and (not finished2):  # post-work执行发生错误
         error_msg += 'post-work running error\n'
 
     if not (finished0 and finished1 and finished2):  # 发生任何错误
         # 发送消息
-        mailSendMsg(
-            """autotest find a error in:
-        error msg: 
-        {0}
-        """.format(error_msg))
-        if cfgfile['iteration']['except_mode'] == 'stop':  # 如果是stop模式则直接退出
-            return False
-        elif cfgfile['iteration']['except_mode'] == 'ignore':  # 忽略,继续执行下一个测试
-            ignore_return = True
-    if ignore_return:
+        SendMsg(
+            """iteration error msg:\n{0}""".format(error_msg))
         return False
     return True
 
@@ -293,12 +265,14 @@ def iteration():
 endless = int(cfgfile['iteration']['num']) < 0
 iterations = int(cfgfile['iteration']['num'])
 while endless or iterations > 0:
-
     finished = iteration()
     iterations -= 1
-
     if not finished:
-        exit(-1)
+        if cfgfile['iteration']['except_mode'] == 'stop':  # 如果是stop模式则直接退出
+            print('**********found tests failed**********')
+            exit(-1)
+        elif cfgfile['iteration']['except_mode'] == 'ignore':  # 忽略,继续执行下一个测试
+            pass
     # 延迟
     time.sleep(eval(cfgfile['iteration']['end_delay']))
 print('**********all tests finished**********')
