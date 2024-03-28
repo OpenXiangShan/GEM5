fa19032238a2c99e89589e4e4821497e76db6469
Yangyu Chen <cyy@cyyself.name>
2024-03-28 11:59:43 +0800

    configs: use realpath for every arguments in parallel_sim.sh
    Since we have cd here, using a relative path in these arguments will not
    work. Wrapping them with realpath will allow users to pass the relative
    path.
    
    Change-Id: I482eb22e63d749297cf1d8888103e3316fbbf59c

--- util/xs_scripts/parallel_sim.sh
+++ util/xs_scripts/parallel_sim.sh
@@ -13,7 +13,7 @@ fi
 
 set -x
 
-export arch_script=$1
+export arch_script=`realpath $1`
 
 # Note 1: workload list contains the workload name, checkpoint path, and parameters, looks like:
 #       astar_biglakes_122060000000 astar_biglakes_122060000000_0.244818/0/ 0 0 20 20
@@ -21,11 +21,11 @@ export arch_script=$1
 # Note 2: The meaning of fields:
 # workload_name, checkpoint_path, skip insts(usually 0), functional_warmup insts(usually 0), detailed_warmup insts (usually 20), sample insts
 # Note 3: you can write a script to generate such a list accordingly
-export workload_list=$2
+export workload_list=`realpath $2`
 
 # The checkpoint directory. We will find checkpoint_path in workload_list
 # under this directory to get the checkpoint path.
-export cpt_dir=$3
+export cpt_dir=`realpath $3`
 
 export tag=$4
 
