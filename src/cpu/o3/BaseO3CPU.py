69f3fca63a2e8064805c270e2f6d4df21ae83c65
tastynoob <934348725@qq.com>
2024-09-26 11:00:01 +0800

    cpu-o3: enlarge StoreSet, reduce mem store latency, fix false bankconflict

--- src/cpu/o3/BaseO3CPU.py
+++ src/cpu/o3/BaseO3CPU.py
@@ -168,11 +168,11 @@ class BaseO3CPU(BaseCPU):
     store_set_clear_period = Param.Unsigned(250000,
             "Number of load/store insts before the dep predictor "
             "should be invalidated")
-    LFSTSize = Param.Unsigned(256, "Last fetched store table size")
+    LFSTSize = Param.Unsigned(2048, "Last fetched store table size")
     store_set_clear_thres = Param.Unsigned(1048576,"")
     LFSTEntrySize = Param.Unsigned(4,"The number of store table inst in every entry of LFST can contain")
-    SSITSize = Param.Unsigned(1024, "Store set ID table size")
-    BankConflictCheck = Param.Bool(True,"open Bank conflict check")
+    SSITSize = Param.Unsigned(8192, "Store set ID table size")
+    BankConflictCheck = Param.Bool(True, "open Bank conflict check")
 
 
     numRobs = Param.Unsigned(1, "Number of Reorder Buffers");
