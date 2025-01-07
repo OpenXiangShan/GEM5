a88cad336908413edbb5461cb6988fe0fcbed5a5
Yan Yue <1131531947@qq.com>
2025-01-07 18:57:59 +0800

    cpu-o3: ideal: fix NumBr = 8
    numBr must be a power of 2, see getShuffledBrIndex()
    find it by valgrind
    
    Change-Id: I6358ac985bae974e356531c5258e7c4f117b1d42

--- configs/example/xiangshan.py
+++ configs/example/xiangshan.py
@@ -344,7 +344,7 @@ def setKmhV3IdealParams(args, system):
         # ideal decoupled frontend
         if args.bp_type is None or args.bp_type == 'DecoupledBPUWithFTB':
             cpu.branchPred.enableTwoTaken = True
-            cpu.branchPred.numBr = 6
+            cpu.branchPred.numBr = 8    # numBr must be a power of 2, see getShuffledBrIndex()
             cpu.branchPred.predictWidth = 64
             cpu.branchPred.tage.enableSC = False # TODO(bug): When numBr changes, enabling SC will trigger an assert
             cpu.branchPred.ftq_size = 256
