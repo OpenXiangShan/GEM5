5b698bf5d23df8d79c7156c5704a913bb66bf981
Xu Boran <xuboran@bosc.ac.cn>
2025-06-27 18:11:52 +0800

    ci: modify ci for testing purpose of this branch
    remember to delete this commit

--- util/xs_scripts/kmh_v3_btb_2taken.sh
+++ util/xs_scripts/kmh_v3_btb_2taken.sh
@@ -7,4 +7,4 @@ for var in GCBV_REF_SO GCB_RESTORER gem5_home; do
     checkForVariable $var
 done
 
-$gem5 $gem5_home/configs/example/xiangshan.py --generic-rv-cpt=$1 --bp-type=DecoupledBPUWithBTB --ideal-kmhv3 --disable-2taken
+$gem5 $gem5_home/configs/example/xiangshan.py --generic-rv-cpt=$1 --bp-type=DecoupledBPUWithBTB --ideal-kmhv3
