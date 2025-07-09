dd5e31245f1fc91797da4b48962d3cf7b6a6871e
Yan Yue <1131531947@qq.com>
2025-07-09 18:20:50 +0800

    cpu-o3: tage: disable 2 ways to save area
    Change-Id: I84be57bd79f651ab62486c7f06433f293bd8c8ab

--- configs/example/xiangshan.py
+++ configs/example/xiangshan.py
@@ -381,7 +381,8 @@ def setKmhV3IdealParams(args, system):
                 cpu.branchPred.predictWidth = 64              # max width of a fetch block
                 cpu.branchPred.btb.numEntries = 16384
                 # TODO: BTB TAGE do not bave base table, do not support SC
-                cpu.branchPred.tage.tableSizes = [2048] * 14  # 2ways, 2048 sets
+                cpu.branchPred.tage.tableSizes = [2048] * 14  # 1 way, 2048 sets
+                cpu.branchPred.tage.numWays = 1
 
             cpu.branchPred.tage.enableSC = False # TODO(bug): When numBr changes, enabling SC will trigger an assert
             cpu.branchPred.ftq_size = 256
