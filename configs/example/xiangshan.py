dc05089b7bb1c5fd7e78ca9425b0569d42fe4bba
Xu Boran <xuboran@bosc.ac.cn>
2025-07-18 17:40:55 +0800

    temporarily disable mgsc, remember to revert

--- configs/example/xiangshan.py
+++ configs/example/xiangshan.py
@@ -383,6 +383,7 @@ def setKmhV3IdealParams(args, system):
                 # TODO: BTB TAGE do not bave base table, do not support SC
                 cpu.branchPred.tage.tableSizes = [2048] * 14  # 2ways, 2048 sets
                 cpu.branchPred.enable2Taken = not args.disable_2taken
+                cpu.branchPred.mgsc.enableMGSC = False;
 
             cpu.branchPred.tage.enableSC = False # TODO(bug): When numBr changes, enabling SC will trigger an assert
             cpu.branchPred.ftq_size = 256
