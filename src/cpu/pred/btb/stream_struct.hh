be323a213f599692ea6d87e651ff7a6b3700e3e3
Easton Man <me@eastonman.com>
2025-08-15 18:09:36 +0800

    cpu-o3: update phr at uncond br
    also use better hash
    
    Change-Id: I43dae0166353a4e3d617e86035bad107e1ecf0d5

--- src/cpu/pred/btb/stream_struct.hh
+++ src/cpu/pred/btb/stream_struct.hh
@@ -635,6 +635,9 @@ struct FullBTBPrediction
                     }
                 } else {
                     // uncond
+                    taken = true;
+                    pc = entry.pc; // get the pc of the cond branch
+                    target = entry.target;
                     break;
                 }
             }
