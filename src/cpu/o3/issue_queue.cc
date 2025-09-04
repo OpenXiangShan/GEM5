b40ae15a5b389a27d5f7fdeafd6d4773acd48cd6
tastynoob <934348725@qq.com>
2025-09-04 10:12:28 +0800

    cpu-o3: fix piped fdiv latency
    Change-Id: Ib2437fb4d49f47c326b2e9fcd699c69eda87a225

--- src/cpu/o3/issue_queue.cc
+++ src/cpu/o3/issue_queue.cc
@@ -1382,7 +1382,7 @@ Scheduler::getOpLatency(const DynInstPtr& inst)
         }
     } else if (inst->opClass() == FloatDivOp) [[unlikely]] {
         if (inst->staticInst->operWid() == 32) {
-            return 7;
+            return 11;
         }
     }
     return opExecTimeTable[inst->opClass()];
