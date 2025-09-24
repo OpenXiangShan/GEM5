496e8934eb6baf7b8b54f82b2990164908986ffd
Easton Man <me@eastonman.com>
2025-09-24 17:10:10 +0800

    cpu-o3: pred reduce MGSC latency to 2 bubble
    Change-Id: I54898085002aec620a485b7728ffa0a59477a762

--- src/cpu/pred/BranchPredictor.py
+++ src/cpu/pred/BranchPredictor.py
@@ -1136,7 +1136,7 @@ class BTBMGSC(TimedBaseBTBPredictor):
     weightTableIdxWidth = Param.Unsigned(5,
         "Log size of weight table")
 
-    numDelay = 3
+    numDelay = 2
 
 class DecoupledBPUWithBTB(BranchPredictor):
     type = 'DecoupledBPUWithBTB'
