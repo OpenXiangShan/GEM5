e97c47f54c0a3594f846ae851ddc19c4b7ec0cf9
Yakkhini <59007159+Yakkhini@users.noreply.github.com>
2025-10-11 18:47:36 +0800

    cpu-o3: adjust I2F delay to suqash before tage resolve train
    This will also affect ldstqCount. Does it matter?
    
    Change-Id: I4f1729d411deebeadd88fbbbb3dba30853487a30

--- configs/example/idealkmhv3.py
+++ configs/example/idealkmhv3.py
@@ -25,6 +25,7 @@ def setKmhV3IdealParams(args, system):
         # fetch
         cpu.mmu.itb.size = 96
         cpu.fetchWidth = 32
+        cpu.iewToFetchDelay = 2
         cpu.commitToFetchDelay = 2
         cpu.fetchQueueSize = 64
         cpu.fetchToDecodeDelay = 2
