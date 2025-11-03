d37b53ea1ccd7c97c023ed6fc5bbfe3b4b7444d6
Yan Yue <1131531947@qq.com>
2025-11-03 17:34:47 +0800

    cpu-o3: use less tage phr length, 400->200
    Change-Id: I3eb025d1787130ebb471d45e2e0ffe14c7d985cd

--- configs/example/xiangshan.py
+++ configs/example/xiangshan.py
@@ -405,7 +405,7 @@ def setKmhV3IdealParams(args, system):
             cpu.branchPred.tage.numPredictors = 8
             cpu.branchPred.tage.TTagBitSizes = [11] * 8
             cpu.branchPred.tage.TTagPcShifts = [1] * 8
-            cpu.branchPred.tage.histLengths = [4, 9, 17, 29, 56, 109, 211, 397]
+            cpu.branchPred.tage.histLengths = [5, 11, 19, 31, 59, 107, 173, 211]
 
         # ideal l1 caches
         if args.caches:
