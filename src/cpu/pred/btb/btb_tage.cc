8c51f9c4569d949e822e404e4b024fab3a6db238
Cao Jiaming <caojiaming@bosc.ac.cn>
2025-11-05 17:34:53 +0800

    cpu-o3: enhance TAGE improved misprediction handling
    Change-Id: I3d272a39a19685c9c31f33b18db7e6488175f479

--- src/cpu/pred/btb/btb_tage.cc
+++ src/cpu/pred/btb/btb_tage.cc
@@ -245,7 +245,8 @@ BTBTAGE::generateSinglePrediction(const BTBEntry &btb_entry,
     // Use base table instead of btb_entry.ctr
     Addr base_idx = getBaseTableIndex(alignedPC);
     unsigned branch_idx = getBranchIndexInBlock(btb_entry.pc, alignedPC);
-    bool base_taken = baseTable[base_idx][branch_idx] >= 0;
+    bool base_taken = getDelay() !=0 ? baseTable[base_idx][branch_idx] >= 0 : btb_entry.ctr >=0;
+                        // if delay=0, use btb_entry.ctr
     bool alt_pred = alt_provided ? alt_taken : base_taken; // if alt provided, use alt prediction, otherwise use base
 
     // use_alt_on_na gating: when provider weak, consult per-PC counter
@@ -482,14 +483,16 @@ BTBTAGE::updatePredictorStateAndCheckAllocation(const BTBEntry &entry,
     }
 
     // check if need to allocate new entry
-    bool this_cond_mispred = stream.squashType == SquashType::SQUASH_CTRL && 
+    bool this_fb_mispred = stream.squashType == SquashType::SQUASH_CTRL &&
                                stream.squashPC == entry.pc;
-    if (this_cond_mispred) {
-        tageStats.updateMispred++;
-        if (!used_alt && main_info.found) {
+    if (getDelay() == 2){
+        if (this_fb_mispred) {
+            tageStats.updateMispred++;
+            if (!used_alt && main_info.found) {
 #ifndef UNIT_TEST
-            tageStats.updateTableMispreds[main_info.table]++;
+                tageStats.updateTableMispreds[main_info.table]++;
 #endif
+            }
         }
     }
 
@@ -498,7 +501,7 @@ BTBTAGE::updatePredictorStateAndCheckAllocation(const BTBEntry &entry,
                                         main_info.taken() == actual_taken;
 
     // return true if need to allocate new entry = mispred and main was incorrect
-    return this_cond_mispred && !use_alt_on_main_found_correct;
+    return this_fb_mispred && !use_alt_on_main_found_correct;
 }
 
 /**
@@ -658,7 +661,32 @@ BTBTAGE::update(const FetchStream &stream) {
         }
 #endif
     }
-
+    if (getDelay() != 2 ){
+        //sort microtage predictions by pc to find the first taken branch
+        std::vector<std::pair<Addr, TagePrediction>> lastPreds;
+        lastPreds.reserve(predMeta->preds.size());
+        for (auto &kv : predMeta->preds) {
+            lastPreds.emplace_back(kv.first, kv.second);
+        }
+        std::sort(lastPreds.begin(), lastPreds.end(),
+                [](const std::pair<Addr, TagePrediction> &a,
+                    const std::pair<Addr, TagePrediction> &b) {
+                    return a.first < b.first;
+                });
+        Addr first_taken_pc = 0;
+        for (auto &entry_info : lastPreds) {
+            if (entry_info.second.taken) {
+                first_taken_pc = entry_info.first;
+                break;
+            }
+        }
+        bool fallthrough_mispred = (first_taken_pc == 0 && stream.exeTaken) ||
+                                    (first_taken_pc != 0 && !stream.exeTaken);
+        bool branch_mispred = stream.exeTaken && first_taken_pc != stream.exeBranchInfo.pc;
+        if (fallthrough_mispred || branch_mispred) {
+            tageStats.updateMispred++;
+    }
+    }
     DPRINTF(TAGE, "end update\n");
 }
 
