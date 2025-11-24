880838dd716c0d19ad4f7cb67d6a5595bc9213ff
Yan Yue <1131531947@qq.com>
2025-11-24 13:18:19 +0800

    cpu-o3: only enable ubtb and mbtb
    Change-Id: Icdd4abdead5301edc317a1befb167ab87ab2238a

--- configs/example/kmhv3.py
+++ configs/example/kmhv3.py
@@ -96,6 +96,15 @@ def setKmhV3Params(args, system):
             cpu.branchPred.ftq_size = 256
             cpu.branchPred.fsq_size = 256
 
+            cpu.branchPred.ubtb.enabled = True
+            cpu.branchPred.abtb.enabled = False
+            cpu.branchPred.microtage.enabled = False
+            cpu.branchPred.mbtb.enabled = True
+            cpu.branchPred.tage.enabled = False
+            cpu.branchPred.ittage.enabled = False
+            cpu.branchPred.mgsc.enabled = False
+            cpu.branchPred.ras.enabled = False
+
         # l1 cache per core
         if args.caches:
             cpu.icache.size = '64kB'
