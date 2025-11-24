6f60e88c51724e11897042fe32e6f5412a278a6c
Yan Yue <1131531947@qq.com>
2025-11-24 15:18:08 +0800

    cpu-o3: align prefetcher in kmhv3.py
    Change-Id: I5be9a5d5ce4a787caafc2d160350bbdc629039fb

--- configs/example/kmhv3.py
+++ configs/example/kmhv3.py
@@ -153,6 +153,7 @@ if __name__ == '__m5_main__':
     # If user didn't specify bp_type, set default based on ideal_kmhv3
     args.bp_type = 'DecoupledBPUWithBTB'
     args.l2_size = '1MB'
+    args.kmh_align = True
 
     # Match the memories with the CPUs, based on the options for the test system
     TestMemClass = Simulation.setMemClass(args)
