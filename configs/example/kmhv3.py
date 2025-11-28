b50a22491171d93d9d70f909add41686bd76e479
Yan Yue <1131531947@qq.com>
2025-11-28 11:34:57 +0800

    cpu-o3: tage, mbtb use resolve update now
    Change-Id: I732d89766545b668bfce47ffe6be9f9b7161d569

--- configs/example/kmhv3.py
+++ configs/example/kmhv3.py
@@ -94,6 +94,9 @@ def setKmhV3Params(args, system):
             cpu.branchPred.ftq_size = 256
             cpu.branchPred.fsq_size = 256
 
+            cpu.branchPred.mbtb.resolvedUpdate = True
+            cpu.branchPred.tage.resolvedUpdate = True
+
             cpu.branchPred.ubtb.enabled = True
             cpu.branchPred.abtb.enabled = False
             cpu.branchPred.microtage.enabled = False
