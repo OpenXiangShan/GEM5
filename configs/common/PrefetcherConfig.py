6ddbea72c9169a961ad3a5373458d9b4cc46b998
Yan Yue <1131531947@qq.com>
2025-12-09 12:07:00 +0800

    cpu-o3: enable cmc in kmh align prefetcher as default config
    Change-Id: I1dae1fe9ae92d8dbdaf7d59b90208765fa88bdb2

--- configs/common/PrefetcherConfig.py
+++ configs/common/PrefetcherConfig.py
@@ -65,7 +65,7 @@ def create_prefetcher(cpu, cache_level, options):
                 prefetcher.enable_bop = True
             if options.kmh_align:
                 assert prefetcher_name == 'L2CompositeWithWorkerPrefetcher'
-                prefetcher.enable_cmc = False
+                prefetcher.enable_cmc = True
                 prefetcher.enable_bop = True
                 prefetcher.enable_cdp = False
                 prefetcher.enable_despacito_stream = False
@@ -83,7 +83,7 @@ def create_prefetcher(cpu, cache_level, options):
                 prefetcher.enable_bop = True
             if options.kmh_align:
                 assert prefetcher_name == 'L2CompositeWithWorkerPrefetcher'
-                prefetcher.enable_cmc = False
+                prefetcher.enable_cmc = True
                 prefetcher.enable_bop = True
                 prefetcher.enable_cdp = False
                 prefetcher.enable_despacito_stream = False
