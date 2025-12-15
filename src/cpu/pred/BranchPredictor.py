a499bbed47f4daded02d0e63f3529a92eb763177
Yan Yue <1131531947@qq.com>
2025-12-15 11:16:55 +0800

    cpu-o3: align to RTL, disable mbtb victimCache now
    Change-Id: I0d422470e967986833d988bc0eae08c4d20b1fd1

--- src/cpu/pred/BranchPredictor.py
+++ src/cpu/pred/BranchPredictor.py
@@ -984,7 +984,7 @@ class MBTB(TimedBaseBTBPredictor):
     numDelay = 2
     blockSize = 32  # max 64 byte block, 32 byte aligned
     # MBTB is always half-aligned - no parameter needed
-    victimCacheSize = Param.Unsigned(16, "Number of entries in the victim cache")
+    victimCacheSize = Param.Unsigned(0, "Number of entries in the victim cache")
 
 class AheadBTB(TimedBaseBTBPredictor):
     type = 'AheadBTB'
