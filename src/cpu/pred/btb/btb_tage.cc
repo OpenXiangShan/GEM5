1de8633082956bb348fd3feb3ebef85e8e18e0cb
Cao Jiaming <caojiaming@bosc.ac.cn>
2025-12-23 18:17:44 +0800

    cpu-o3: simplify base prediction logic in BTBTAGE
    Change-Id: Ieb052b87c6479122f89454d1c6f69fa7ef5f6f8c

--- src/cpu/pred/btb/btb_tage.cc
+++ src/cpu/pred/btb/btb_tage.cc
@@ -269,7 +269,8 @@ BTBTAGE::generateSinglePrediction(const BTBEntry &btb_entry,
     // Use base table instead of btb_entry.ctr
     Addr base_idx = getBaseTableIndex(startPC);
     unsigned branch_idx = getBranchIndexInBlock(btb_entry.pc, startPC);
-    bool base_taken = getDelay() != 0 ? baseTable[base_idx][branch_idx] >= 0 : btb_entry.ctr >= 0;
+    //bool base_taken = getDelay() != 0 ? baseTable[base_idx][branch_idx] >= 0 : btb_entry.ctr >= 0;
+    bool base_taken = btb_entry.ctr >= 0;
     bool alt_pred = alt_provided ? alt_taken : base_taken; // if alt provided, use alt prediction, otherwise use base
 
     // use_alt_on_na gating: when provider weak, consult per-PC counter
