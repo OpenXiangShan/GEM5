20f8daf59c25bbf9e8dcaf3810fa85d86406fd18
Yan Yue <1131531947@qq.com>
2025-12-24 18:50:21 +0800

    bpu: align BTBTAGE tag  to support bank conflict handling
    test use
    [tag: 13 bits] [setIdx: 10 bits] [bankIdx: 2 bits] [instOffset: 1 bit]
    
    Change-Id: I0b9e667cf13c7f1990bf06ad0e01bb64bba0ded9

--- src/cpu/pred/btb/btb_tage.cc
+++ src/cpu/pred/btb/btb_tage.cc
@@ -812,7 +812,8 @@ BTBTAGE::getTageTag(Addr pc, int t, uint64_t foldedHist, uint64_t altFoldedHist,
     // Create mask for tableTagBits[t] to limit result size
     Addr mask = (1ULL << tableTagBits[t]) - 1;
 
-    const unsigned pcShift = enableBankConflict ? indexShift : bankBaseShift;
+    unsigned pcShift = enableBankConflict ? indexShift : bankBaseShift;
+    pcShift += tableIndexBits[t] - 1;   // since tableIndexBits = log(2048) = 11, RTL is 10
     Addr pcBits = (pc >> pcShift) & mask;
 
     // Extract and prepare folded history bits
