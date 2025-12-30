13b722296ffb20ecaec0e45051d2a6096ff7e902
edwu186 <wuchengkai1998@gmail.com>
2025-12-30 16:11:06 +0800

    mem: set9-6-perf
    Change-Id: I72681deac9666f9d524fb65fc2e5c80ecec2e480

--- configs/example/idealkmhv3.py
+++ configs/example/idealkmhv3.py
@@ -85,6 +85,7 @@ def setKmhV3IdealParams(args, system):
             cpu.dcache.size = '64kB'
             cpu.dcache.tag_load_read_ports = 100
             cpu.dcache.mshrs = 16
+            cpu.dcache.simulate_dcache_refill = True
 
     # l2 caches
     if args.l2cache:
