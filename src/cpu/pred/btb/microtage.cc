a358334647f9c177f70e167cd1344a397cd3678c
Cao Jiaming <caojiaming@bosc.ac.cn>
2026-01-08 16:17:35 +0800

    cpu-o3: remove unused variable 'usingBasetable'
    Change-Id: I8be037a1cdacd7151f4fe8e743a32cca90a85036

--- src/cpu/pred/btb/microtage.cc
+++ src/cpu/pred/btb/microtage.cc
@@ -91,7 +91,6 @@ indexShift(bankBaseShift + ceilLog2(p.numBanks)),
 enableBankConflict(p.enableBankConflict),
 lastPredBankId(0),
 predBankValid(false),
-usingBasetable( !p.usingMbtbBaseEiterTage),
 tageStats(this, p.numPredictors, p.numBanks)
 {
     this->needMoreHistories = p.needMoreHistories;
