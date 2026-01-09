faaa09e7318162fb5fe66deb9b8458ba68b29aba
edwu186 <wuchengkai1998@gmail.com>
2026-01-09 15:09:30 +0800

    mem: SPECtest1
    Change-Id: Ib17430ae5dbf9e69d81c60db027d1a85bbefd04c

--- configs/example/idealkmhv3.py
+++ configs/example/idealkmhv3.py
@@ -31,7 +31,7 @@ def setKmhV3IdealParams(args, system):
 
         # decode
         cpu.decodeWidth = 8
-        cpu.enable_loadFusion = False
+        cpu.enable_loadFusion = True
         cpu.enableConstantFolding = False
 
         # rename
@@ -73,7 +73,7 @@ def setKmhV3IdealParams(args, system):
         cpu.RAWDequeuePerCycle = 4
         cpu.SbufferEntries = 24
         cpu.SbufferEvictThreshold = 16
-        cpu.store_prefetch_train = False
+        cpu.store_prefetch_train = True
 
         # branch predictor
         if args.bp_type == 'DecoupledBPUWithBTB':
