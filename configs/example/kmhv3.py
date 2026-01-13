8caa6526c706107b47d5b45b4080c7fddc7d40cd
Ergou-ren <291056892@qq.com>
2026-01-13 11:32:28 +0800

    mem: close dispatch for test
    Change-Id: I5b9b715b9cfde2a39ce41d9ca1aaf49c6d3ab4c4

--- configs/example/kmhv3.py
+++ configs/example/kmhv3.py
@@ -38,7 +38,6 @@ def setKmhV3Params(args, system):
         cpu.renameWidth = 8
         cpu.numPhysIntRegs = 224
         cpu.numPhysFloatRegs = 256
-        cpu.enable_storeSet_train = False
 
         # dispatch
         cpu.enableDispatchStage = False
