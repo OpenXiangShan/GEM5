ed95dbf106f4f6c4ea6b87a16eba1f2fc75ca684
Cao Jiaming <caojiaming@bosc.ac.cn>
2026-01-23 11:13:03 +0800

    cpu-o3: disable MicroTAGE in branch prediction configuration
    Change-Id: I56614e8ebc2dd33320d353562087ab456fc452da

--- configs/example/idealkmhv3.py
+++ configs/example/idealkmhv3.py
@@ -79,7 +79,7 @@ def setKmhV3IdealParams(args, system):
         if args.bp_type == 'DecoupledBPUWithBTB':
             cpu.branchPred.ftq_size = 256
             cpu.branchPred.fsq_size = 256
-            cpu.branchPred.microtage.enabled = False
+            # cpu.branchPred.microtage.enabled = False
 
         # l1 cache per core
         if args.caches:
