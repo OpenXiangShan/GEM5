5871427e72c5e30a65c3ed6cd23e2850924b2ddf
Yan Yue <1131531947@qq.com>
2026-01-27 14:52:50 +0800

    bpu: sc: try to use bigger threshold
    from max 1024 to max 4096
    
    Change-Id: Ic303d37dabcf4d4ee37dc8f8093cb65ad83de32a

--- src/cpu/pred/BranchPredictor.py
+++ src/cpu/pred/BranchPredictor.py
@@ -1126,7 +1126,7 @@ class BTBMGSC(TimedBaseBTBPredictor):
     thresholdTablelogSize = Param.Unsigned(6,
         "Log size of update threshold counters tables")
 
-    updateThresholdWidth = Param.Unsigned(12,
+    updateThresholdWidth = Param.Unsigned(14,
         "Number of bits for the update threshold counter")
 
     pUpdateThresholdWidth = Param.Unsigned(8,
