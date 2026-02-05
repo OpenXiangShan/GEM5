e8840fe8a8357df070503179c2cd123c2dac969f
Llemonade <609628381@qq.com>
2026-02-05 19:48:25 +0800

    mem : Test disable BOP
    Change-Id: I362fc8870daa1f52f279481451e043c0e11120c4

--- configs/common/PrefetcherConfig.py
+++ configs/common/PrefetcherConfig.py
@@ -97,7 +97,7 @@ def create_prefetcher(cpu, cache_level, options):
             if options.kmh_align:
                 assert prefetcher_name == 'L2CompositeWithWorkerPrefetcher'
                 prefetcher.enable_cmc = True
-                prefetcher.enable_bop = True
+                prefetcher.enable_bop = False
                 prefetcher.enable_cdp = False
                 prefetcher.enable_despacito_stream = True
                 if prefetcher.enable_despacito_stream:
