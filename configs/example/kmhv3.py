b6bf5af9a83a3bcadff6eb34e249ab7273981ad5
Yan Yue <1131531947@qq.com>
2026-03-03 16:16:11 +0800

    bpu: tage not enable bank conflict
    Change-Id: Ie8aef098adea8dab3d0dd7db34fa163517a850d7

--- configs/example/kmhv3.py
+++ configs/example/kmhv3.py
@@ -99,6 +99,8 @@ def setKmhV3Params(args, system):
             cpu.branchPred.tage.resolvedUpdate = True
             cpu.branchPred.ittage.resolvedUpdate = True
 
+            cpu.branchPred.tage.enableBankConflict = False
+
             cpu.branchPred.ubtb.enabled = True
             cpu.branchPred.abtb.enabled = True
             cpu.branchPred.microtage.enabled = False
