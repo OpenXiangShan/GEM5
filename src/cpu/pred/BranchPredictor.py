8d5b2a959c152766c1a42f6fea2c50bbb38232d2
Cao Jiaming <caojiaming@bosc.ac.cn>
2026-03-09 10:28:09 +0800

    cpu-o3: change tage longth to 15 15 16 16
    Change-Id: I9153ff6ead4c95fe01ae64aedc7942117b50e7d4

--- src/cpu/pred/BranchPredictor.py
+++ src/cpu/pred/BranchPredictor.py
@@ -1072,7 +1072,7 @@ class MicroTAGE(TimedBaseBTBPredictor):
     # Keep vector parameters consistent with numPredictors to avoid constructor asserts.
     numPredictors = Param.Unsigned(4, "Number of TAGE predictors")
     tableSizes = VectorParam.Unsigned([512] * 4,"the TAGE T0~Tn length")
-    TTagBitSizes = VectorParam.Unsigned([16] * 4 ,"the T0~Tn entry's tag bit size")
+    TTagBitSizes = VectorParam.Unsigned([15,15,16,16],"the T0~Tn entry's tag bit size")
     TTagPcShifts = VectorParam.Unsigned([1] * 4 ,"when the T0~Tn entry's tag generating, PC right shift")
     blockSize = Param.Unsigned(32,"tage index function uses 32B aligned block address")
 
