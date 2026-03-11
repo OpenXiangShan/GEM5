84ec451b1889c76b1cb5bd11b88a563f29cc1466
Cao Jiaming <caojiaming@bosc.ac.cn>
2026-03-11 10:53:44 +0800

    cpu-o3:  MicroTAGE predictor parameters for enhanced performance
    Change-Id: I608fe975cf564882034081f632cbf72ef1d78181

--- src/cpu/pred/BranchPredictor.py
+++ src/cpu/pred/BranchPredictor.py
@@ -1070,13 +1070,13 @@ class MicroTAGE(TimedBaseBTBPredictor):
     enableSC = Param.Bool(False, "Enable SC or not")
     updateOnRead = Param.Bool(True,"Enable update on read, no need to save tage meta in FTQ")
     # Keep vector parameters consistent with numPredictors to avoid constructor asserts.
-    numPredictors = Param.Unsigned(8, "Number of TAGE predictors")
-    tableSizes = VectorParam.Unsigned([2048]*8 ,"the TAGE T0~Tn length")
-    TTagBitSizes = VectorParam.Unsigned([13]*8  ,"the T0~Tn entry's tag bit size")
-    TTagPcShifts = VectorParam.Unsigned([1]*8  ,"when the T0~Tn entry's tag generating, PC right shift")
+    numPredictors = Param.Unsigned(4, "Number of TAGE predictors")
+    tableSizes = VectorParam.Unsigned([2048]*4 ,"the TAGE T0~Tn length")
+    TTagBitSizes = VectorParam.Unsigned([13]*4  ,"the T0~Tn entry's tag bit size")
+    TTagPcShifts = VectorParam.Unsigned([1]*4  ,"when the T0~Tn entry's tag generating, PC right shift")
     blockSize = Param.Unsigned(32,"tage index function uses 32B aligned block address")
 
-    histLengths = VectorParam.Unsigned([4, 9, 17, 29, 56, 109, 211, 397] ,"the BTB TAGE T0~Tn history length")
+    histLengths = VectorParam.Unsigned([5, 9, 17, 29] ,"the BTB TAGE T0~Tn history length")
     maxHistLen = Param.Unsigned(970,"The length of history passed from DBP")
     numTablesToAlloc = Param.Unsigned(1,"The number of table to allocated each time")
     numWays = Param.Unsigned(2, "Number of ways per set")
