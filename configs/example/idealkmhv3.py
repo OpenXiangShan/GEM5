32f100789798571311692a8f9bd47fa01b5b20fa
edwu186 <wuchengkai1998@gmail.com>
2026-03-31 15:05:59 +0800

    mem: L3 64MB
    Change-Id: I292239d5875ecf9accf655c7e3e77bef4f828076

--- configs/example/idealkmhv3.py
+++ configs/example/idealkmhv3.py
@@ -139,7 +139,7 @@ if __name__ == '__m5_main__':
     args.bp_type = 'DecoupledBPUWithBTB'
     args.l2_size = '2MB'
 
-    args.l3_size = '32MB'
+    args.l3_size = '64MB'
 
     # Match the memories with the CPUs, based on the options for the test system
     TestMemClass = Simulation.setMemClass(args)
