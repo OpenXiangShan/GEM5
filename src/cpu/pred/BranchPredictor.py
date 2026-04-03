4f98b643b56bdfff84622e8cbe9c234ba8a04e71
LiuJingping <liujingping221@mails.ucas.ac.cn>
2026-04-03 17:05:34 +0800

    bpu: tagepercep 1024-7-5-27-1
    Change-Id: I6543c5e85bfe4bbec8d8689a0d6ee66289b0f8a9

--- src/cpu/pred/BranchPredictor.py
+++ src/cpu/pred/BranchPredictor.py
@@ -1162,8 +1162,8 @@ class BTBMGSC(TimedBaseBTBPredictor):
     enablePTable = Param.Bool(False, "Enable P (path) table")
     enableBiasTable = Param.Bool(False, "Enable Bias table")
     enablePCThreshold = Param.Bool(False, "Enable PC-indexed threshold table")
-    enablePerceptronPred = Param.Bool(False, "Enable perceptron predictor")
-    forceUsePercep = Param.Bool(False,"Use tage as input of perceptron predictor")
+    enablePerceptronPred = Param.Bool(True, "Enable perceptron predictor")
+    forceUsePercep = Param.Bool(True,"Use tage as input of perceptron predictor")
 
     numDelay = 2
 
