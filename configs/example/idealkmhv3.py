ad8ab1cee4536b8c4f28e950a71b1351b12752b4
Yakkhini <59007159+Yakkhini@users.noreply.github.com>
2026-04-10 11:28:24 +0800

    misc: enable resovle train on idealkmhv3 for evaluation
    Change-Id: I9bb3f593b1e247977b371103fdd5ebb66821fcaa

--- configs/example/idealkmhv3.py
+++ configs/example/idealkmhv3.py
@@ -86,6 +86,9 @@ def setKmhV3IdealParams(args, system):
             cpu.branchPred.ftq_size = 64
             cpu.branchPred.fsq_size = 64
             # cpu.branchPred.microtage.enabled = False
+            cpu.branchPred.mbtb.resolvedUpdate = True
+            cpu.branchPred.tage.resolvedUpdate = True
+            cpu.branchPred.ittage.resolvedUpdate = True
 
         # l1 cache per core
         if args.caches:
