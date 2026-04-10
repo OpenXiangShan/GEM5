52305c8e1135d0dee90faafd36c661fde4f40e80
Cao Jiaming <CaoJ0017@e.ntu.edu.sg>
2026-04-10 11:07:11 +0800

    Tage trace fix (#822)[skip ci]
    * cpu: update prediction latency in MicroTAGE from 0 to 1 cycle
    
    Change-Id: I7c0b359c8241119f090b2092766e56fa0174464c
    
    * cpu-o3: disable update on read in BTBTAGE and refactor trace
    
    Change-Id: Ic43dffba4ad2fd78e78f5e70b16529069a034676
    
    * Revert "cpu: update prediction latency in MicroTAGE from 0 to 1 cycle"
    
    This reverts commit 369a95270ff328ac6ba81bf55b12e1de47fbfd93.
    
    ---------
    
    Co-authored-by: Cao Jiaming <caojiaming@bosc.ac.cn>

--- src/cpu/pred/BranchPredictor.py
+++ src/cpu/pred/BranchPredictor.py
@@ -1042,7 +1042,7 @@ class BTBTAGE(TimedBaseBTBPredictor):
 
     needMoreHistories = Param.Bool(True, "BTBTAGE needs more histories")
     enableSC = Param.Bool(False, "Enable SC or not")    # TODO: BTBTAGE doesn't support SC
-    updateOnRead = Param.Bool(True, "Enable update on read, no need to save tage meta in FTQ")
+    updateOnRead = Param.Bool(False, "Enable update on read, no need to save tage meta in FTQ")
     numPredictors = Param.Unsigned(8, "Number of TAGE predictors")
     tableSizes = VectorParam.Unsigned([2048, 2048, 2048, 2048, 2048, 2048, 2048,2048],"the TAGE T0~Tn length")
     TTagBitSizes = VectorParam.Unsigned([13] * 8, "the T0~Tn entry's tag bit size")
