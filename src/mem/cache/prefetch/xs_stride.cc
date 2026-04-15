071b8698294c9f121478ae3da5728134bdc4054d
XXtaoo <gy88880000@gmail.com>
2026-04-15 11:12:49 +0800

    mem: l1 stride 8 depth and l2 stride 64 depth
    Change-Id: I7235a2c757707e9c2832b7eefb7646e6370a6fc9

--- src/mem/cache/prefetch/xs_stride.cc
+++ src/mem/cache/prefetch/xs_stride.cc
@@ -380,7 +380,7 @@ XSStridePrefetcher::triggerFromCommitTable(const PrefetchInfo &pfi,
     }
 
     if (useXsDepth) {
-        sendPFWithFilter(pfi, blockAddress(lookupAddr + (entry->stride << 2)),
+        sendPFWithFilter(pfi, blockAddress(lookupAddr + (entry->stride << 3)),
                          addresses, 0, PrefetchSourceType::SStride, 1);
         sendPFWithFilter(pfi, blockAddress(lookupAddr + (entry->stride << 6)),
                          addresses, 0, PrefetchSourceType::SStride, 2);
