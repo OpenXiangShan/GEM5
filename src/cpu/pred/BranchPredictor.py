ae46818673dfe0ee343e775d7245923758d16aff
Cao Jiaming <caojiaming@bosc.ac.cn>
2026-04-21 19:58:50 +0800

    cpu: Tune BTBTAGE local candidate 1
    Change-Id: I9aa4bed9b5789f04eddb92ce9005d7ab9c2aef23

--- src/cpu/pred/BranchPredictor.py
+++ src/cpu/pred/BranchPredictor.py
@@ -1044,7 +1044,7 @@ class BTBTAGE(TimedBaseBTBPredictor):
     enableSC = Param.Bool(False, "Enable SC or not")    # TODO: BTBTAGE doesn't support SC
     updateOnRead = Param.Bool(False, "Enable update on read, no need to save tage meta in FTQ")
     numPredictors = Param.Unsigned(8, "Number of TAGE predictors")
-    tableSizes = VectorParam.Unsigned([2048, 2048, 2048, 4096, 2048, 2048, 1024, 1024],"the TAGE T0~Tn length")
+    tableSizes = VectorParam.Unsigned([512, 1024, 2048, 4096, 2048, 4096, 4096, 512],"the TAGE T0~Tn length")
     TTagBitSizes = VectorParam.Unsigned([13, 13, 13, 13, 13, 13, 15, 15], "the T0~Tn entry's tag bit size")
     TTagPcShifts = VectorParam.Unsigned([1] * 8, "when the T0~Tn entry's tag generating, PC right shift")
     blockSize = 32 # tage index function uses 32B aligned block address
@@ -1052,7 +1052,7 @@ class BTBTAGE(TimedBaseBTBPredictor):
     histLengths = VectorParam.Unsigned([4, 9, 17, 29, 56, 109, 211,397],"the BTB TAGE T0~Tn history length")
     maxHistLen = Param.Unsigned(970, "The length of history passed from DBP")
     numTablesToAlloc = Param.Unsigned(1,"The number of table to allocated each time")
-    numWays = VectorParam.Unsigned([2] * 8,"the T0~Tn number of ways per set")
+    numWays = VectorParam.Unsigned([2, 2, 1, 4, 3, 1, 1, 2],"the T0~Tn number of ways per set")
     maxBranchPositions = Param.Unsigned(32, "Maximum branch positions per 64-byte block")
     useAltOnNaSize = Param.Unsigned(128, "Size of the useAltOnNa table")
     useAltOnNaWidth = Param.Unsigned(7, "Width of the useAltOnNa table")
