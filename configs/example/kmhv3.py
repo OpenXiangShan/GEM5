44ea6ae8ae0e82e3f474c5c89b52831f0276e22d
Yan Yue <1131531947@qq.com>
2026-05-07 17:50:52 +0800

    configs: Enable only MGSC IMLI table
    Change-Id: I2e585ec892473c4b83747f05018cf2464a0d5f52

--- configs/example/kmhv3.py
+++ configs/example/kmhv3.py
@@ -113,8 +113,8 @@ def setKmhV3Params(args, system):
             cpu.branchPred.mgsc.enabled = True
             # SC table ablation config for gcc15 slice CI.
             cpu.branchPred.mgsc.enableBwTable = False
-            cpu.branchPred.mgsc.enableLTable = True
-            cpu.branchPred.mgsc.enableITable = False
+            cpu.branchPred.mgsc.enableLTable = False
+            cpu.branchPred.mgsc.enableITable = True
             cpu.branchPred.mgsc.enableGTable = False
             cpu.branchPred.mgsc.enablePTable = False
             cpu.branchPred.mgsc.enableBiasTable = False
