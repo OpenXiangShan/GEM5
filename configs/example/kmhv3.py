5ba51e68e8ef9aef7f7615d4e2b58fcbad536841
lixin <1037997956@qq.com>
2026-05-07 10:42:56 +0800

    mem: set l3 as 32M in kmhv3
    Change-Id: I97ce2a0a01f9759e5978c52d718cc3659118d2d2

--- configs/example/kmhv3.py
+++ configs/example/kmhv3.py
@@ -188,6 +188,7 @@ if __name__ == '__m5_main__':
     # If user didn't specify bp_type, set default based on ideal_kmhv3
     args.bp_type = 'DecoupledBPUWithBTB'
     args.l2_size = '2MB'
+    args.l3_size = '32MB'
     args.kmh_align = True   # align prefetcher in RTL, spec06 decrease 1 score
 
     # Match the memories with the CPUs, based on the options for the test system
