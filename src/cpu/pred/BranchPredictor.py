31ef8480ce09159bdc9ed92b94c34597726f73fa
Cao Jiaming <caojiaming@bosc.ac.cn>
2026-05-14 16:01:39 +0800

    cpu: Increase default share table ways
    Change-Id: Ic68b859389ca330f0e517989c8de091eba8246e9

--- src/cpu/pred/BranchPredictor.py
+++ src/cpu/pred/BranchPredictor.py
@@ -1055,7 +1055,7 @@ class BTBTAGE(TimedBaseBTBPredictor):
     numWays = VectorParam.Unsigned([2] * 8,"the T0~Tn number of ways per set")
     enableShareTable = Param.Bool(True, "Enable runtime-bound share table")
     shareTableSize = Param.Unsigned(2048, "Share table entries per set-index domain")
-    shareTableWays = Param.Unsigned(2, "Number of ways in the share table")
+    shareTableWays = Param.Unsigned(6, "Number of ways in the share table")
     shareAllocWindow = Param.Unsigned(1400, "Alloc-success count per share FSM window")
     shareAllocConsecutive = Param.Unsigned(2, "Consecutive winning windows needed to bind share table")
     maxBranchPositions = Param.Unsigned(32, "Maximum branch positions per 64-byte block")
