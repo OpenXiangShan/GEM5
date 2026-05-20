1d2a555f52d0c43a465afcc63185ce5a0210a5c1
tastynoob <934348725@qq.com>
2026-05-20 10:35:52 +0800

    arch-riscv: Isolate old TLB privilege by thread
    Change-Id: Ie6f7cfc4bd2d2d8f7aad6c9e7bace19534b77c00

--- src/arch/riscv/tlb.hh
+++ src/arch/riscv/tlb.hh
@@ -34,6 +34,7 @@
 
 #include <cstdint>
 #include <list>
+#include <vector>
 
 #include "arch/generic/tlb.hh"
 #include "arch/riscv/isa.hh"
@@ -42,6 +43,7 @@
 #include "arch/riscv/regs/misc.hh"
 #include "arch/riscv/utility.hh"
 #include "base/statistics.hh"
+#include "base/types.hh"
 #include "mem/request.hh"
 #include "params/RiscvTLB.hh"
 #include "sim/sim_object.hh"
@@ -107,9 +109,13 @@ class TLB : public BaseTLB
     uint64_t lastPc;
     uint64_t traceFlag;
 
-    bool use_old_priv;
-    PrivilegeMode old_priv_ldst;
-    PrivilegeMode old_priv_ex;
+    struct OldPrivState
+    {
+        bool valid = false;
+        PrivilegeMode ldst = PrivilegeMode::PRV_M;
+    };
+
+    std::vector<OldPrivState> oldPrivByThread;
 
     Walker *walker;
 
@@ -253,6 +259,7 @@ class TLB : public BaseTLB
                               BaseMMU::Translation *translation, BaseMMU::Mode mode) override;
     Fault finalizePhysical(const RequestPtr &req, ThreadContext *tc,
                            BaseMMU::Mode mode) const override;
+    PrivilegeMode currentMemPriv(ThreadContext *tc, BaseMMU::Mode mode);
     TlbEntry *lookup(Addr vpn, uint16_t asid, BaseMMU::Mode mode, bool hidden,
                      bool sign_used, uint8_t translateMode,
                      bool is_prefetch = false);
@@ -262,14 +269,8 @@ class TLB : public BaseTLB
     TlbEntry *lookupL2TLB(Addr vpn, uint16_t asid, BaseMMU::Mode mode, bool hidden, int f_level, bool sign_used,
                           uint8_t translateMode);
 
-    void setOldPriv(ThreadContext *tc) {
-      use_old_priv = true;
-      old_priv_ex = getMemPriv(tc, BaseMMU::Execute);
-      old_priv_ldst = getMemPriv(tc, BaseMMU::Read);
-    }
-    void useNewPriv(ThreadContext *tc) {
-      use_old_priv = false;
-    }
+    void setOldPriv(ThreadContext *tc);
+    void useNewPriv(ThreadContext *tc);
 
 
     std::vector<TlbEntry> tlbL2L3;  // our TLB
