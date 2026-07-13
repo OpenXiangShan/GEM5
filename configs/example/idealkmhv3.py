f45840d4331d0249af34272b73378851394d8667
lixin <1037997956@qq.com>
2026-07-13 10:11:23 +0800

    misc: use solver parameters
    Change-Id: If89bf471becbc828a8fd61a859da22143fb264bd

--- configs/example/idealkmhv3.py
+++ configs/example/idealkmhv3.py
@@ -106,8 +106,8 @@ def setKmhV3IdealParams(args, system):
             cpu.branchPred.ftq_size = 64
             cpu.branchPred.fsq_size = 64
             # TAGE table sizes and numWays tunning
-            cpu.branchPred.tage.tableSizes = [2048, 2048, 8192, 8192, 8192, 8192, 8192, 2048]
-            cpu.branchPred.tage.numWays = [2, 2, 4, 2, 2, 2, 2, 2]
+            cpu.branchPred.tage.tableSizes = [2048, 4096, 8192, 8192, 8192, 4096, 8192, 4096]
+            cpu.branchPred.tage.numWays = [5, 7, 5, 8, 8, 8, 8, 8]
             # cpu.branchPred.microtage.enabled = False
 
         # l1 cache per core
