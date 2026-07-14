590e1f06704ef0cc13f4c9e3c2b5e42d8f9586fe
weiding liu <1045251744@qq.com>
2026-07-14 10:49:42 +0800

    mem: open mdp strict predict
    Change-Id: If0b74998357c10a40eb0c7c74fdfcc0f3b19dbfa

--- src/cpu/o3/BaseO3CPU.py
+++ src/cpu/o3/BaseO3CPU.py
@@ -211,7 +211,7 @@ class BaseO3CPU(BaseCPU):
     EnableReplayBasedMDP = Param.Bool(True,
         "Use replay-based mem dependency prediction (loads don't stall in IQ, "
         "but may replay in load pipe)")
-    EnableMDPStrictWait = Param.Bool(False,
+    EnableMDPStrictWait = Param.Bool(True,
         "Enable StoreSet strict-wait in mem dep prediction (checkInstStrict)")
 
     numPhysIntRegs = Param.Unsigned(224,
