5d76b8517829e030232d0e73b1495b0011a36a48
Cao Jiaming <caojiaming@bosc.ac.cn>
2026-07-20 17:44:00 +0800

    bpu: Match ABTB associativity with RTL
    Change-Id: Ia1c0f7ad7b7e9127d93a6b7edab120cfba045c01

--- src/cpu/pred/BranchPredictor.py
+++ src/cpu/pred/BranchPredictor.py
@@ -1001,7 +1001,7 @@ class AheadBTB(TimedBaseBTBPredictor):
     tagBits = Param.Unsigned(38, "Number of bits in the tag")
     instShiftAmt = Param.Unsigned(1, "Amount to shift PC to get inst bits")
     numThreads = Param.Unsigned(Parent.numThreads, "Number of threads")
-    numWays = Param.Unsigned(8, "Number of ways per set")
+    numWays = Param.Unsigned(4, "Number of ways per set")
     aheadPipelinedStages = Param.Unsigned(1, "Number of stages ahead pipelined")
     entryHalfAligned = Param.Bool(False, "Whether the entries are half-aligned")
     blockSize = 64
