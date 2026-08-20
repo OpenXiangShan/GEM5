e1223fdbbfa38e7e9b46b5ae48a060cb669f9169
Frankslu <1653954675@qq.com>
2026-08-20 18:17:00 +0800

    cpu: Set 1mb l2
    Change-Id: Ib2ca66363491b52e55218a62322988781150f1f2

--- configs/example/idealkmhv3.py
+++ configs/example/idealkmhv3.py
@@ -165,7 +165,7 @@ if __name__ == '__m5_main__':
     # Set default bp_type based on ideal_kmhv3 flag
     # If user didn't specify bp_type, set default based on ideal_kmhv3
     args.bp_type = 'DecoupledBPUWithBTB'
-    args.l2_size = '2MB'
+    args.l2_size = '1MB'
     args.l3_size = '32MB'
     # Match the memories with the CPUs, based on the options for the test system
     TestMemClass = Simulation.setMemClass(args)
--- configs/example/kmhv3.py
+++ configs/example/kmhv3.py
@@ -201,7 +201,7 @@ if __name__ == '__m5_main__':
     # Set default bp_type based on ideal_kmhv3 flag
     # If user didn't specify bp_type, set default based on ideal_kmhv3
     args.bp_type = 'DecoupledBPUWithBTB'
-    args.l2_size = '2MB'
+    args.l2_size = '1MB'
     args.kmh_align = True   # align prefetcher in RTL, spec06 decrease 1 score
 
     # Match the memories with the CPUs, based on the options for the test system
