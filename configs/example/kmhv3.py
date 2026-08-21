65fd42f623d254968b4d51d1b58a95621902c20a
caojiaming <caojiaming@bosc.ac.cn>
2026-08-21 12:46:35 +0800

    configs: Re-enable MGSC in kmhv3 BTB
    Reverts commit 3332f20df235b07e0868b927f2426bc3671db525.
    
    Change-Id: I7ebd49ed0a82ed62a19c1db2ae0f1eff0e003ef3

--- configs/example/kmhv3.py
+++ configs/example/kmhv3.py
@@ -127,7 +127,7 @@ def setKmhV3Params(args, system):
             cpu.branchPred.mbtb.enabled = True
             cpu.branchPred.tage.enabled = True
             cpu.branchPred.ittage.enabled = True
-            cpu.branchPred.mgsc.enabled = False
+            cpu.branchPred.mgsc.enabled = True
             cpu.branchPred.ras.enabled = True
 
             if getattr(args, 'standalone_sc', False):
