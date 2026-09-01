1c2f06e82052780d2078ed04690caec322af83cd
Yan Yue <1131531947@qq.com>
2026-09-01 17:07:50 +0800

    cpu-pred: Align RAS capacity with RTL (#1097)
    * cpu: Align RAS committed capacity with RTL
    
    Change-Id: I67777f71840f3816575ebe20334f88d0d0373637
    
    * cpu: Model RTL RAS speculative queue limit
    
    Use logical speculative pointers to preserve full/empty state across ring wrap, limit the queue to 32 entries, and block speculative updates at the RTL near-overflow threshold. Add occupancy/blocking statistics and a focused recovery test.
    
    Change-Id: Ia75c3d99bb974cdce5e013d481ea06afe0101a29
    
    * cpu: Align RAS speculative retirement window
    
    Change-Id: I03f5ba10fc926eb68b525f230e27cb4eb427fe8a

--- src/cpu/pred/BranchPredictor.py
+++ src/cpu/pred/BranchPredictor.py
@@ -1031,9 +1031,9 @@ class BTBRAS(TimedBaseBTBPredictor):
     cxx_header = 'cpu/pred/btb/ras.hh'
 
     numThreads = Param.Unsigned(Parent.numThreads, "Number of threads")
-    numEntries = Param.Unsigned(32, "Number of entries in the RAS")
-    ctrWidth = Param.Unsigned(8, "Width of the counter")
-    numInflightEntries = Param.Unsigned(384, "Number of inflight entries")
+    numEntries = Param.Unsigned(16, "Number of entries in the RAS")
+    ctrWidth = Param.Unsigned(3, "Width of the counter")
+    numInflightEntries = Param.Unsigned(32, "Number of inflight entries")
     numDelay = 2
 
 class BTBuRAS(TimedBaseBTBPredictor):
